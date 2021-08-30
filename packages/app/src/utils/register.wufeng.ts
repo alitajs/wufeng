7e9d44fe98d33ea55a52ac5afe4a378f4833ab31
xiaohuoni <448627663@qq.com>
2021-08-30 14:36:00 +0800

    chore: add wufeng utils

--- packages/app/src/utils/register.wufeng.ts
+++ packages/app/src/utils/register.wufeng.ts
@@ -1,37 +1,23 @@
 import { dynamic } from 'alita';
-import { wufengController } from 'wufeng';
+import { wufengController, dashToPascalCase } from 'wufeng';
 import * as Yinhu from '@alita/react';
 import { getItemByType } from './data_utils';
 import { DataComponet } from './data_componet';
 
 const antdMobile = ['accordion', 'action-sheet', 'button', 'icon'];
 
-function lineToHump(s: string) {
-  const a = s.split('-');
-  let result = a[0].slice(0, 1).toUpperCase() + a[0].slice(1);
-  for (let i = 1; i < a.length; ) {
-    result = result + a[i].slice(0, 1).toUpperCase() + a[i].slice(1);
-    i += 1;
-  }
-  return result;
-}
-
-function humpToLine(name: string) {
-  return name.replace(/([A-Z])/g, '-$1').toLowerCase();
-}
-
 antdMobile.forEach((item: string) => {
-  const options = getItemByType(DataComponet, lineToHump(item)) as any;
+  const options = getItemByType(DataComponet, dashToPascalCase(item)) as any;
   if (options?.type) {
     wufengController.registerComponent(
       dynamic(() => import(`antd-mobile/lib/${item}/index`).then((res) => res.default as any)),
-      { name: lineToHump(item), ...options },
+      { name: dashToPascalCase(item), ...options },
     );
   }
 });
 
 Object.keys(Yinhu).forEach((item: string) => {
-  const options = getItemByType(DataComponet, lineToHump(item)) as any;
+  const options = getItemByType(DataComponet, dashToPascalCase(item)) as any;
   if (options?.type) {
     wufengController.registerComponent(Yinhu[item], { name: `YH${item}`, ...options });
   }
--- packages/wufeng/src/index.ts
+++ packages/wufeng/src/index.ts
@@ -1,13 +1,14 @@
 import { WuFeng } from './WuFeng';
 
+const wufeng = new WuFeng();
+WuFeng.singletonInstance = wufeng;
+export { wufeng as wufengController };
+
 export { default as WFPage } from './Page';
 export { default as WFComponentsWare } from './ComponentsWare';
 export { default as WFPhoneFrame } from './PhoneFrame';
 export { default as DataSource } from './DataSource';
 
-const wufeng = new WuFeng();
-WuFeng.singletonInstance = wufeng;
-
-export { wufeng as wufengController };
+export * from './utils';
 export * from '@alitajs/dnd';
 export * from '@wufeng/types';
